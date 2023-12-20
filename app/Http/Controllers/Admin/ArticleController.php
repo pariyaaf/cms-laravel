5d837e38c35d161ab64a6108102301dfbf7d9403
pariyaaf <pariyaafsharpour79@gmail.com>
2023-12-20 15:08:33 +0330

    compalte edit delete update store index

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\Admin\ArticleRequest;
+use App\Traits\FileUploadTrait;
+
 use App\Models\Article;
 use  Illuminate\View\View;
 
 
 class ArticleController extends Controller
 {
+    use FileUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -30,9 +34,30 @@ class ArticleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ArticleRequest $request)
     {
-        dd($request->all());
+        try{
+                $images = $request->file('images');
+                $imagesUrl = $this->uploadImage($images);
+
+                // dd($request->all(), array_merge($request->all() , [ 'images' => $imagesUrl]));
+                
+                auth()->loginUsingId(6);
+                $request->images = $imagesUrl;
+                auth()->user()->article()->create(
+
+                    array_merge($request->all() , [ 'images' => $imagesUrl])
+                                );
+
+                return back();//  زمانی که به صورت پست باشه میشه ازش استفاده کرد.
+
+                // return redirect()->route('Admin.articles.create');
+
+        } catch(\Exception $e){
+            dd($e);
+            return back();//  زمانی که به صورت پست باشه میشه ازش استفاده کرد.
+            // return redirect(route('articles.index'));
+        }
     }
 
     /**
@@ -46,17 +71,41 @@ class ArticleController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
-        //
+        $article = Article::findOrFail($id);
+        $sized = ['original', 'small', 'medium', 'large'];
+        return view('Admin.articles.edit',compact('article','sized'));
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(ArticleRequest $request, string $id)
     {
-        //
+        $input = $request->all();
+        $article =Article::findOrFail($id);
+        try{
+            if($request->file('images')){
+                $images = $request->file('images');
+                $input['images'] = $this->uploadImage($images);
+                $this->RemoveFile($article->images);
+
+            }
+            else {
+                $input['images'] = $article->images;
+                $input['images']['thumb'] = $input['imagesThumb'];
+            }
+      
+
+            unset($input['imagesThumb']);
+            $article->update($input);
+            $articles = Article::latest()->paginate(20);
+            return view('Admin.articles.index',compact('articles'));
+} catch(\Exception $e) {
+            return back();
+        }
     }
 
     /**
@@ -64,6 +113,10 @@ class ArticleController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $article = Article::findOrFail($id);
+        $this->RemoveFile($article->images);
+        $article->delete();
+        return back();//  زمانی که به صورت پست باشه میشه ازش استفاده کرد.
+
     }
 }
