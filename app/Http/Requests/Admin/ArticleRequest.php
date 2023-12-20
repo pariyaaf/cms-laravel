17a22a318fb726dc7aba9ec0969f3594838add59
pariyaaf <pariyaafsharpour79@gmail.com>
2023-12-20 15:06:04 +0330

    create requst to handle validation of articles

---
+++ app/Http/Requests/Admin/ArticleRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        if($this->method() == 'POST') {
+            return [
+                'title' =>['required ','max:250'],
+                'body' => ['required '] ,
+                'description' => ['required '] ,
+                'images' => ['required ', 'mimes:png,jpg,jpeg,bmp'],
+                'tags' => ['required '],
+                //
+            ];
+        } 
+        return [
+            'title' =>['required ','max:250'],
+            'body' => ['required '] ,
+            'description' => ['required '] ,
+            'images' => ['mimes:png,jpg,jpeg,bmp'],
+            'tags' => ['required '],
+            //
+        ];
+        
+       
+    }
+}
