fd515ab87b9267701daf6aa3ba978e0fbfff5879
pariyaaf <pariyaafsharpour79@gmail.com>
2023-12-31 20:02:30 +0330

    add necessery items

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,8 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Str;
 
 class Course extends Model
 {
+    use Sluggable;
+
     use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'images' => 'array'
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'//منبع اسلاگ رو مشخص میکنیم.
+            ]
+        ];
+    } 
+
+    public function path ()
+    {
+        return "/course/$this->slug";
+
+    }
+
+
+    public function setBodyAttribute($value)
+    {
+        $content = preg_replace('/<[^>]*>/' , '' , $value);
+        $this->attributes['description'] = Str::limit($content, 500);
+        $this->attributes['body'] = $value;
+    }
+
+
 }
