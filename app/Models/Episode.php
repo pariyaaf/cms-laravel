9f564d98aa8c6c17aaab2fb56fa267d44301f91c
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-01 22:01:38 +0330

    manage episode

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Course;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Str;
 
-class Epicode extends Model
+class Episode extends Model
 {
+    use Sluggable;
+
     use HasFactory;
-}
+
+    protected $guarded = [];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'//منبع اسلاگ رو مشخص میکنیم.
+            ]
+        ];
+    } 
+
+
+    public function path ()
+    {
+        return "/course/{$this->course->slug}/epicode/{$this->episodeNumber}";
+
+    }
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }}
