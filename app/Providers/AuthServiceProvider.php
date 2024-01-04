eacdac57196a6f2170dd942f8453ad4ef5a71783
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-04 16:34:56 +0330

    modify boot to add gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use Illuminate\Support\Facades\Gate;
+use App\Models\Article;
+use App\Models\User;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -21,6 +23,30 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+        Gate::define('update-post', function (User $user, Article $course) {
+            return $user === $course->user_id;
+        });
+
+
+        // Gate::define('show-comment', function (User $user) {
+        //     return $user->hasRole(Permissioin::whereName('show-comment')->first()->roles);
+        // });
+   
+            $this->registerPolicies();
+    
+            foreach ($this->getPermissions() as $permission) {
+                Gate::define($permission->name , function ($user) use($permission){
+                    return $user->hasRole($permission->roles);
+                });
+            }
+    
+        }
+    
+        protected function getPermissions()
+        {
+            return Permission::with('roles')->get();
+        }
+
+
+    
 }
