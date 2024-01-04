b6db561273a89a2cb388c9475016e1ac4e3a5569
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-04 16:33:34 +0330

    add new relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,27 @@ class User extends Authenticatable
     {
         return $this->hasMany(Course::class);
     }
+
+    
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        if(is_string($role)) {
+            return $this->roles->contains('name' , $role);
+        }
+//
+//        foreach ($role as $r) {
+//            if($this->hasRole($r->name)) {
+//                return true;
+//            }
+//        }
+//        return false;
+
+        return !! $role->intersect($this->roles)->count();
+    }
+    
 }
