c2f49f3f4bf3f1efcd3e732196bed750838f736e
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-12 10:44:58 +0330

    handle gate and ..

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use App\Models\Article;
+use App\Models\Permission;
 use App\Models\User;
 class AuthServiceProvider extends ServiceProvider
 {
