fec5948e9b142ee4b32391510848e38dde030812
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-14 22:44:07 +0330

    create table for paymebts

---
+++ database/migrations/2024_01_14_164842_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');// couse it hase fk 
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->string('resnumber');// to check payment
+            $table->integer('course_id')->default(0);
+            $table->string('price');// check price
+            $table->boolean('payment')->default(false);// success or not
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
