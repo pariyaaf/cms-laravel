a1bcaeef07ad0b4ba9fc74a61fbde7410bdc5749
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-18 15:48:20 +0330

    add new event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        'App\Events\UserActivation\UserActivation' => [
+            'App\Listeners\UserActivation\SendMailNotification',
+        ],
+    
     ];
 
     /**
