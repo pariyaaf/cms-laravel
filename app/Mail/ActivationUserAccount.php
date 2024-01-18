962a0645539280697f78c7f04e547c5ae0a6a136
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-18 15:39:50 +0330

    create  mail in laravel for handle emails send

---
+++ app/Mail/ActivationUserAccount.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class ActivationUserAccount extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $code;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(User $user , $code)
+    {
+        $this->user = $user;
+        $this->code = $code;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'لینک فعالسازی',
+        );
+
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            // view: 'view.name',
+            markdown: 'emails.active-user',
+        );
+       
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ app/Mail/TestMail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.test')->with(['content' => 'This is a test email']);
+    }
+}
