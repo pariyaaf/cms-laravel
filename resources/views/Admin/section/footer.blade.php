e88afb0143634f62900680b4bd82701845cf6bc2
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-18 15:44:33 +0330

    handle for graphs

--- resources/views/Admin/section/footer.blade.php
+++ resources/views/Admin/section/footer.blade.php
@@ -4,7 +4,6 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="../resources/js/app.js"></script>
 <script src="/js/bootstrap-select.min.js"></script>
-
 @yield('script')
