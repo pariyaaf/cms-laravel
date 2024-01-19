2c21cd70f42b2fcab9684e881065121633f693c5
pariyaaf <pariyaafsharpour79@gmail.com>
2024-01-19 11:29:24 +0330

    create recaptcha validator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\Validator;
+use GuzzleHttp\Client;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +21,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);    }
+        Schema::defaultStringLength(191);   
+        Validator::extend('recaptcha', function ($attribute, $value, $params, $vaidator){
+            $client = new Client();
+            $res = $client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
+                'form_params' => [
+                    'secret' => config('services.recaptcha.secret'),
+                    'response' => $value,
+                    'remoteip' => request()->ip(),
+                ]
+            ]);
+
+            if ($res->getStatusCode() == 200) {
+               $result = json_decode( $res->getbody());
+                return $result->success;
+
+            }
+            return false;
+
+        });
+    }
 }
