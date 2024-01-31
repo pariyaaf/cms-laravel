8fa465ba88870bb54ad4b1924beff7f02ecb09ed
pariyaaf <pariyaafsharpour79@gmail.com>
2024-02-01 00:51:07 +0330

    login with api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\ArticleController;
+use App\Http\Controllers\Api\v1\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('allUser', function() {
+
+});
+//seprate base on user version
+Route::group(['prefix'=>'v1', 'as'=>'v1.','namespace' => 'App\Http\Controllers\Api\v1'], function() {
+    Route::get('articles', [ArticleController::class,'articles']);
+    Route::post('comments', [ArticleController::class,'comments']);
+    Route::middleware('auth:sanctum')->group(function() {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
+
+
+    Route::post('login',[UserController::class,'login']);
 });
