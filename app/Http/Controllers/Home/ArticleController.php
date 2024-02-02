e32d3fd751471472761d136f294b8bb5f90be0dc
pariyaaf <pariyaafsharpour79@gmail.com>
2024-02-02 20:58:02 +0330

    handle view count and return views

--- app/Http/Controllers/Home/ArticleController.php
+++ app/Http/Controllers/Home/ArticleController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Article;
+use  Illuminate\View\View;
+use Illuminate\Support\Facades\Redis;
+
 
 class ArticleController extends Controller
 {
-    //
+    public function single(Article $article): View {
+        $article->increment('viewCount');
+        Redis::Incr("views.{$article->id}.articles");
+        return view('Home.article');    }
 }
--- app/Http/Controllers/Home/CourseController.php
+++ app/Http/Controllers/Home/CourseController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Course;
+use  Illuminate\View\View;
+use Illuminate\Support\Facades\Redis;
 
 class CourseController extends Controller
 {
-    //
+    public function single(Course $course) :View {
+        $course->increment('viewCount');
+        Redis::Incr("views.{$course->id}.courses");
+        return view('Home.courses');
+    }
 }
