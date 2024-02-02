795f2662f0690a9635336f37f244881117a87c0e
pariyaaf <pariyaafsharpour79@gmail.com>
2024-02-02 19:07:40 +0330

    create new controller

---
+++ app/Http/Controllers/Home/HomeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Course;
+use Carbon\Carbon;
+
+use Cache;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // cache()->pull('courses');
+        if(cache()->has('articles')) {
+            $articles = cache('articles');
+        } else {
+            $articles = Article::latest()->take(8)->get();
+            cache(['articles'=> $articles, Carbon::now()->addDays(1)]);
+        }
+
+        if(cache()->has('courses')) {
+            $courses = cache('courses');
+        } else {
+            $courses = Course::latest()->take(4)->get();
+            cache(['courses'=> $courses, Carbon::now()->addDays(1)]);
+        }
+
+        return view('Home.index', compact('articles','courses'));
+    }
+}
