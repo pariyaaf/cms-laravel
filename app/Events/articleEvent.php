c4c4e73b365433a532303b5748f4e6095f7163de
pariyaaf <pariyaafsharpour79@gmail.com>
2024-02-02 16:23:06 +0330

    create envent to handle pusher

---
+++ app/Events/articleEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class articleEvent implements ShouldBroadcast///////  i added
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $user;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): Channel
+    {
+        // dd( new PrivateChannel('articles.admin'));
+
+        return new PrivateChannel('articles.admin');
+    }
+
+    // public function broadcastWith() : array {
+    //     return [
+    //         'massage'=> 'this is broadcast',
+    //     ];
+    // }
+
+}
