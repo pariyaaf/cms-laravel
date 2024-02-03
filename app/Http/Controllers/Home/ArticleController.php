015a13420c55de1c6fc9755afc587916391b9d2c
pariyaaf <pariyaafsharpour79@gmail.com>
2024-02-03 22:06:26 +0330

    sent comment and subcomment to view

--- app/Http/Controllers/Home/ArticleController.php
+++ app/Http/Controllers/Home/ArticleController.php
@@ -7,12 +7,16 @@ use Illuminate\Http\Request;
 use App\Models\Article;
 use  Illuminate\View\View;
 use Illuminate\Support\Facades\Redis;
+use App\Models\Comment;
 
 
 class ArticleController extends Controller
 {
     public function single(Article $article): View {
+
+        $comments = $article->comments()->where('approved',1)->where('parent_id',0)->latest()->with('comments')->get();
         $article->increment('viewCount');
         Redis::Incr("views.{$article->id}.articles");
-        return view('Home.article');    }
+        return view('Home.article', compact('article','comments'));
+        }
 }
--- app/Http/Controllers/Home/CourseController.php
+++ app/Http/Controllers/Home/CourseController.php
@@ -11,8 +11,11 @@ use Illuminate\Support\Facades\Redis;
 class CourseController extends Controller
 {
     public function single(Course $course) :View {
+
+        $comments = $course->comments()->where('approved',1)->where('parent_id',0)->latest()->with('comments')->get();
+
         $course->increment('viewCount');
         Redis::Incr("views.{$course->id}.courses");
-        return view('Home.courses');
+        return view('Home.courses',compact('course', 'comments'));
     }
 }
