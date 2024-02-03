0de114e1504a6f535b92cb2b4a24b7aea7f3ae35
pariyaaf <pariyaafsharpour79@gmail.com>
2024-02-03 22:07:36 +0330

    add bootstrap

--- resources/views/Home/master.blade.php
+++ resources/views/Home/master.blade.php
@@ -13,6 +13,8 @@
 
     <!-- Bootstrap Core CSS -->
     <link href="/css/home.css" rel="stylesheet">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <body>
 
