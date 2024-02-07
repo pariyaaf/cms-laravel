6d0ffb3e9b7e28ce25e4d8a57814196b6d47f9af
pariyaaf <pariyaafsharpour79@gmail.com>
2024-02-07 21:23:47 +0330

    add view for comments

---
+++ resources/views/Admin/comments/unapproved.blade.php
@@ -0,0 +1,47 @@
+@extends('Admin.master')
+
+@section('content')
+    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <div class="page-header head-section">
+            <h2> نظرات تایید نشده</h2>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>نام کاربر</th>
+                    <th>متن کامنت</th>
+                    <th>پست مربوطه</th>
+                    <th>تنظیمات</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($comments as $comment)
+                    <tr>
+                        <td>{{ $comment->user->name }}</td>
+                        <td>{{ $comment->comment }}</td>
+                        <td><a href="{{ $comment->commentable->path() }}">{{  $comment->commentable->title }}</a></td>
+                        <td>
+                        <div style="display: flex">
+                        <form action="{{route('admin.comments.destroy' ,$comment->id)}}" method="POST">
+                                @csrf
+                        @method('delete')
+                                    <button type="submit" class="btn btn-xs btn-danger">حذف</button>
+                                </form>
+                                <form action="{{route('admin.comments.update' ,$comment->id)}}" method="POST">
+                                @csrf
+                        @method('patch')
+                                    <button type="submit" class="btn btn-xs btn-success">تایید</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div style="text-align: center">
+            {!! $comments->render() !!}
+        </div>
+    </div>
+@endsection
