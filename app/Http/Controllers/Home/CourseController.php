ffa1ba0e648f40e606726ae99ec365a427255e50
pariyaaf <pariyaafsharpour79@gmail.com>
2024-02-07 21:25:24 +0330

    none

--- app/Http/Controllers/Home/CourseController.php
+++ app/Http/Controllers/Home/CourseController.php
@@ -7,13 +7,14 @@ use Illuminate\Http\Request;
 use App\Models\Course;
 use  Illuminate\View\View;
 use Illuminate\Support\Facades\Redis;
+use App\Models\Comment;
+
 
 class CourseController extends Controller
 {
     public function single(Course $course) :View {
 
         $comments = $course->comments()->where('approved',1)->where('parent_id',0)->latest()->with('comments')->get();
-
         $course->increment('viewCount');
         Redis::Incr("views.{$course->id}.courses");
         return view('Home.courses',compact('course', 'comments'));
